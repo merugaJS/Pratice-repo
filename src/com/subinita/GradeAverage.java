14db6c2ba735fadd975bd7003e8e0fa55e07eecb
subidey <121973222+subidey@users.noreply.github.com>
2023-08-08 13:08:53 -0400

    want to add files

---
+++ src/com/subinita/GradeAverage.java
@@ -0,0 +1,39 @@
+package com.subinita;
+
+import java.util.Scanner;
+public class GradeAverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner scanner = new Scanner(System.in);
+
+		        int totalGrades = 0;
+		        int numberOfGrades = 0;
+
+		        System.out.println("Enter grades (enter a negative number to calculate the average):");
+
+		        while (true) {
+		            int grade = scanner.nextInt();
+
+		            if (grade < 0) {
+		                break;
+		            }
+
+		            totalGrades += grade;
+		            numberOfGrades++;
+		        }
+
+		        if (numberOfGrades == 0) {
+		            System.out.println("No grades entered. Cannot calculate the average.");
+		        } else {
+		            double average = (double) totalGrades / numberOfGrades;
+		            System.out.println("Average grade: " + average);
+		        }
+
+		        scanner.close();
+		    
+
+	}
+
+}
