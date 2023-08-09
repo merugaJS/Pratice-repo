7002d6c505e3ad135e84ccb7aec2f7d448609c83
vamsi451 <vamsipeddakotla@gmail.com>
2023-08-09 12:49:30 -0400

    added the two classes that consists of functions and execution

---
+++ src/com/vamshi/javaprogramplementation.java
@@ -0,0 +1,12 @@
+package com.vamshi;
+
+public class javaprogramplementation extends Javaprog{
+
+	public static void main(String[] args) {
+		javaprogramplementation j = new javaprogramplementation();
+		System.out.println(j.averagegrade());
+        System.out.println("CheckLeapYear"+' '+j.CheckLeapYear());
+        System.out.println(j.factorial());
+	}
+
+}
